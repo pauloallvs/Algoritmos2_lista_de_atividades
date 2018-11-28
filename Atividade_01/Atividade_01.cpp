f2e478a680ed56559bf53caef0972be09329e9dc
Paulo Alves <pauloallvs@gmail.com>
2018-11-28 16:28:46 -0200

    exercicio 3

--- Atividade_01/Atividade_01.cpp
+++ Atividade_01/Atividade_01.cpp
@@ -1,46 +1,57 @@
-// Atividade_01.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "math.h"
-#include "cmath"
 
-
-
-double lerNumero() {
-	
-	double num;
-
-	printf("Favor digitar um numero: ");
-	scanf_s("%d", &num);
+int leitura()
+{
+	int num;
+	printf("informe sua idade em dias:");
+	scanf_s("%i", &num);
 
 	return num;
+
 }
 
-double distancia(double x1, double x2, double y1, double y2) {
+int calculoAnos(int num)
+{
+	int anos;
 
-	double d;
+	anos = num / 365;
+	return anos;
 
-	double p1 = x2 - x1;
-	double p2 = y2 - y1;
-
-	return 	d = sqrt(pow(p1, 2) + pow(p2, 2));
 }
+int calculoMeses(int num)
+{
+	int meses;
 
+	meses = (num % 365) / 30;
+	return meses;
+
+}
+int calculoDias(int num)
+{
+	int dias;
+
+	dias = (num % 365) % 30;
+	return dias;
+
+}
 
 int main()
 {
 	
-	double a = lerNumero();
-	double b = lerNumero();
-	double c = lerNumero();
-	double d = lerNumero();
-	double total = distancia(a, b, c, d);
-
-	printf_s("A distancia eh %i\n:", total);
-
+	int numdias = leitura();
+	int a = calculoAnos(numdias);
+	int m = calculoMeses(numdias);
+	int d = calculoDias(numdias);
+	printf_s("A idade em anos, meses e dias eh: %i", a);
+	printf_s(" anos ");
+	printf_s("%i", m);
+	printf_s(" meses ");
+	printf_s("%i", d);
+	printf_s(" dias ");
+	
 	system("pause");
 }
-
